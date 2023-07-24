9de9cd0b2b7ff0492fe19eb368bdd38e47823434
Nguyễn Đức Mạnh <nguyenducmanh1101@gmail.com>
2023-07-24 22:55:05 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import Sidenav from './components/Sidenav'
-
+import Main from './components/Main'
 function App() {
   
   return (
       <div>
-        <Sidenav />        
+        <Sidenav />     
+        <Main />   
       </div>
   )
 }
---
+++ src/components/Main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Main = () => {
+  return (
+    <div id='main'>
+        <img className='' src="https://images.unsplash.com/photo-1518770660439-4636190af475?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80" alt=''/>
+    </div>
+  )
+}
+
+export default Main
