4a9ac83d2922d977f42bef0282fad66d14ef1fff
Nguyễn Đức Mạnh <nguyenducmanh1101@gmail.com>
2023-07-25 09:04:11 +0700

    start up

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import { useState } from 'react'
 import Sidenav from './components/Sidenav'
 import Main from './components/Main'
+import Work from './components/Work'
+
 function App() {
   
   return (
       <div>
         <Sidenav />     
-        <Main />   
+        <Main />  
+        <Work /> 
       </div>
   )
 }
---
+++ src/components/Work.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const data = [
+    {
+        year: 2019,
+        title: "Sai Gon University",
+        duration: "5 Years",
+        details: "In previous years, try to study and get 3 scholarships. In the final year of university, look for an internship company to complete the study program and graduate."
+    }
+]
+const Work = () => {
+  return (
+    <div id='work' className='max-w-[1040px] m-auto md:pl-20 p-4 py-16'>
+        <h1 className='text-4xl font-bold text-center text-[#001b5e]'>Work</h1>
+    </div>
+  )
+}
+
+export default Work
